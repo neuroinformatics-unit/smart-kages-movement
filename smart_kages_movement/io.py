9b8745c89e8cfaaf77372fc671c69a49ffefee1d
niksirbi <niko.sirbiladze@gmail.com>
2025-05-26 22:03:46 +0100

    adjusted column names and indexing for the dataframe

--- smart_kages_movement/io.py
+++ smart_kages_movement/io.py
@@ -26,11 +26,11 @@ def parse_data_into_df(data_dir: Path) -> pd.DataFrame:
       output by DeepLabCut.
     - Then, construct the corresponding video file paths based on the
       datetime information in the pose file names.
-    - Finally, create a DataFrame with the kage, date, time, pose file path,
+    - Finally, create a DataFrame with the kage, date, hour, pose file path,
       video file path, and a boolean indicating if the video exists.
 
-    The DataFrame will have a multi-index with kage and datetime,
-    allowing for easy access.
+    The DataFrame will have a multi-index with
+    levels `kage`, `date`, and `hour`.
 
     Parameters
     ----------
@@ -63,15 +63,16 @@ def parse_data_into_df(data_dir: Path) -> pd.DataFrame:
         parts = pose_file.stem.split("_")
         pose_dict["kage"] = parts[0]
         pose_dict["date"] = parts[1]
-        pose_dict["time"] = parts[2].strip("DLC")
-        pose_dict["datetime"] = pd.to_datetime(
-            f"{pose_dict['date']} {pose_dict['time']}",
+        start_time = parts[2].strip("DLC")
+        pose_dict["hour"] = start_time[:2]
+        pose_dict["start_datetime"] = pd.to_datetime(
+            f"{pose_dict['date']} {start_time}",
             format="%Y%m%d %H%M%S",
         )
         pose_dict["pose_file_path"] = pose_file
 
         video_path = _get_video_path(
-            data_dir / pose_dict["kage"], pose_dict["datetime"]
+            data_dir / pose_dict["kage"], pose_dict["start_datetime"]
         )
         pose_dict["video_exists"] = video_path.exists()
         pose_dict["video_file_path"] = video_path
@@ -82,12 +83,10 @@ def parse_data_into_df(data_dir: Path) -> pd.DataFrame:
     # Create DataFrame first
     df = pd.DataFrame(list_of_dicts)
 
-    # Convert columns to correct types
-    df = df.astype(
-        {"kage": str, "date": str, "time": str, "video_exists": bool}
-    )
+    # Enforce str type for certain columns
+    df = df.astype({"kage": str, "date": str, "hour": str})
 
-    # Path columns are already Path objects, no need to convert
+    # Check that all video file paths are unique
     assert df["video_file_path"].is_unique, "Video file paths are not unique."
 
     # Check how many pose files lack corresponding videos
@@ -99,11 +98,54 @@ def parse_data_into_df(data_dir: Path) -> pd.DataFrame:
             "pose files are missing corresponding videos."
         )
 
-    # Sort the DataFrame by kage and datetime
-    df = df.sort_values(by=["kage", "datetime"])
+    # Sort the DataFrame by kage, date, and hour
+    df = df.sort_values(by=["kage", "date", "hour"])
     # Set multi-index for easier access
-    df.set_index(["kage", "datetime"], inplace=True)
+    df.set_index(["kage", "date", "hour"], inplace=True)
     return df
+
+
+def load_corrected_timestamps(data_dir):
+    """
+    Load corrected timestamps from all kage directories.
+
+    The timestamps are stored per day, in a file named
+    "corrected_timestamps.pkl". This file contains a dictionary mapping
+    each pose .h5 file to an array of corrected timestamps.
+
+    Parameters
+    ----------
+    data_dir : Path
+        The path to the directory containing kage data.
+        It must contain subdirectories named like "kageN" where N is
+        an integer indicating the kage number.
+
+    Returns
+    -------
+    dict
+        A dictionary with corrected timestamps for each pose file.
+        The keys are the pose file paths, and the values are arrays of
+        corrected timestamps.
+
+    """
+    TS_FILENAME = "corrected_timestamps.pkl"
+    timestamps = {}
+
+    kage_date_dirs = sorted(data_dir.glob("kage*/analysis/dlc_output/*/*/*"))
+    # exclude dirs whose last component is not a day of the form DD
+    kage_date_dirs = [
+        d for d in kage_date_dirs if d.name.isdigit() and len(d.name) == 2
+    ]
+
+    for kage_date_dir in kage_date_dirs:
+        ts_file = kage_date_dir / TS_FILENAME
+        if ts_file.exists():
+            with ts_file.open("rb") as f:
+                ts_data = pd.read_pickle(f)
+                timestamps.update(ts_data)
+        else:
+            print(f"Warning: could not find {TS_FILENAME} in {kage_date_dir}")
+    return timestamps
 
 
 def _get_video_path(kage_dir, datetime: pd.Timestamp) -> Path:

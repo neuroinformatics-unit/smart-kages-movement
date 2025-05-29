d2b101d3b1c3e67711d2c5e26b603c4b31017489
niksirbi <niko.sirbiladze@gmail.com>
2025-05-29 19:27:19 +0100

    wrote function for generating speed plots

--- smart_kages_movement/plots.py
+++ smart_kages_movement/plots.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import pandas as pd
 import xarray as xr
 
 
@@ -47,5 +48,53 @@ def plot_confidence_hist_by_keypoint(
     plt.suptitle("Confidence Histograms by Keypoint")
     plt.tight_layout()
 
+    if save_path:
+        plt.savefig(save_path, dpi=128)
+
+
+def plot_speed(
+    speed: xr.DataArray,
+    save_path: Path | None = None,
+) -> None:
+    """
+    Plot speed over time and a histogram of the speed values.
+
+    Parameters
+    ----------
+    ds: xr.DataArray
+        The data array containing body speed data.
+    save_path: Path | None
+        Optional path to save the plot. If None, the plot will not be saved.
+
+    """
+    fig, (ax, ax_hist) = plt.subplots(
+        1, 2, figsize=(12, 4), gridspec_kw={"width_ratios": [4, 1]}
+    )
+
+    speed.plot.line(x="time", lw=0.5, ax=ax)
+    ax.set_title("Speed over time")
+    ax.set_ylabel("Speed (cm/sec)")
+    ax.set_xlabel("Datetime")
+    ax.set_xlim(
+        speed.time.min().values,
+        speed.time.max().values,
+    )
+    # set x-ticks to every day
+    plt.xticks(
+        pd.date_range(
+            start=speed.time.min().values,
+            end=speed.time.max().values,
+            freq="1D",
+        ),
+        rotation=90,
+    )
+
+    # Plot histogram of speed
+    speed.plot.hist(bins=25, orientation="horizontal", ax=ax_hist)
+    ax_hist.set_title("Histogram")
+    ax_hist.set_xscale("log")
+    ax_hist.set_xlabel("log count")
+
+    plt.tight_layout()
     if save_path:
         plt.savefig(save_path, dpi=128)
